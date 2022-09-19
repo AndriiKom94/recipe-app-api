ea1b751f46ceef3a258d2e0ee997e53a4a7c9fb7
AndriiKom94 <andrii.kom@ukr.net>
2022-09-19 14:12:36 +0300

    add psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,17 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """
+    Add takes two numbers and returns their sum.
+    """
+    return x+y
+
+
+def subtract(x, y):
+    """
+    Subtract x from y and return result
+    """
+    return y-x
